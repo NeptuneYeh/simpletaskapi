9f888901ee3c8af033c80539d342b118d434351a
ext-alexy <alex_yeh@trendmicro.com>
2024-03-14 12:56:59 +0800

    Fix: update task request binding status only 0,1

--- internal/app/requests/updateTask.request.go
+++ internal/app/requests/updateTask.request.go
@@ -3,6 +3,6 @@ package requests
 type UpdateTaskRequest struct {
 	ID     string `uri:"id" binding:"required"`
 	Name   string `json:"name"`
-	Status int    `json:"status"` // 0: incomplete, 1: complete
+	Status int    `json:"status" binding:"oneof=0 1"` // 0: incomplete, 1: complete
 	Detail string `json:"detail"`
 }
